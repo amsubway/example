88ac0f0c6b9d79dd2ca0542a27521218f5f4f1ee
amsubway <amsubway@163.com>
2016-08-05 00:16:01 +0800

    猜数字

--- GuessNumApp/GuessNumApp/ViewController.swift
+++ GuessNumApp/GuessNumApp/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var MessageLable: UILabel!
+    @IBOutlet var GuessField: UITextField!
+    
+    var correct = arc4random_uniform(5)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func GuessBtn(sender: UIButton) {
+        let guessNum = GuessField.text!
+        let correctNum = String(correct)
+        
+        if guessNum == correctNum {
+            MessageLable.text = "You get it!"
+            MessageLable.textColor = UIColor.greenColor()
+        }else{
+            MessageLable.text = "Wrong Answer"
+            MessageLable.textColor = UIColor.redColor()
+        }
+        
+        
+    }
 
 }
 
