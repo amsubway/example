c9f006e3eb86ea70ad9ef6d9bd05377ced55b0e9
amsubway <amsubway@163.com>
2016-08-04 14:18:46 +0800

    add project GuessNumApp

--- GuessNumApp/GuessNumApp/ViewController.swift
+++ GuessNumApp/GuessNumApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  SayHiApp
+//  GuessNumApp
 //
 //  Created by 温良 on 16/8/4.
 //  Copyright © 2016年 subway. All rights reserved.
@@ -10,14 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet  var lbName: UILabel!
-    @IBOutlet  var fieldName: UITextField!
-    
-    @IBAction func btnSayHi(sender: UIButton) {
-        let name = fieldName.text!
-        lbName.text = "hi there,\(name)"
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
